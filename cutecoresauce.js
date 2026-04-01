767af8613890e148ceefc7f8c0d5938d38a045df
elliegari <eleanoraproctor@gmail.com>
2026-04-01 11:49:19 -0400

    cutecoresauce.js
    A stupid mod made by a child still figuring out GitHub.

---
+++ cutecoresauce.js
@@ -0,0 +1,40 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\margl1440\margr1440\vieww28600\viewh14200\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 // To create a mod:\
+// Create a new Javascript file like this one.\
+// Add the file to the mods folder on GitHub, or host it somewhere else.\
+// https://github.com/R74nCom/sandboxels/tree/main/mods\
+\
+// To learn about modding, check the wiki: https://sandboxels.wiki.gg/wiki/Modding\
+// Or join our Discord: https://r74n.com/discord/\
+\
+// To add it in the Mod Manager:\
+// If it is in the mods folder, you can just use the name of the file. (example_mod.js)\
+// If it is hosted somewhere else, you can use the full URL, including the HTTPS://.\
+\
+// Adding elements:\
+elements.cutecore_sauce = \{\
+    color: "#ff00dd,\
+    behavior: behaviors.LIQUID,\
+    category: \'93weapons\'94,\
+    state: "liquid",\
+    density: 1100,\
+    viscosity: 60000,\
+\}\
+\
+elements.goth_sauce = \{\
+	color: \'93#434343,\
+	behavior: behaviors.LIQUID,\
+	category: \'93aesthetics\'94,\
+	state: \'93liquid\'94,\
+\pard\pardeftab720\partightenfactor0
+\cf0 \outl0\strokewidth0 	density: 1100,\
+	viscosity: 60000,\
+\}\
